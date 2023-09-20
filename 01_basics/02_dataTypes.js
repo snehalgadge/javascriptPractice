2fac7d3e446906b2512d1f169760da14176a6daf
Snehal Gadge <91423583+snehalgadge@users.noreply.github.com>
2023-09-20 14:03:23 +0000

    datatpes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+// primitive
+let name = "Snehal"  //string
+let age = 23        //number & for big number bigInt
+let email = true    //boolean
+let marks = null   //null
+let grad;
+
+console.table( [typeof name, typeof age, typeof email, typeof marks, typeof grad])
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,4 @@
+let age = "23";
+let ageInNumber = Number(age);
+console.log(typeof age);
+console.log(typeof ageInNumber);
